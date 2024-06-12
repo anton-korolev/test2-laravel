993b529d28ab4aa963b5a1e466c3e86b4f6edab7
Anton Korolev <antonkorolev.dev@gmail.com>
2024-06-12 17:05:54 +0300

    Add routes to test articles

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\ArticleController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/article', [ArticleController::class, 'index']);
+Route::get('/article/{id}', [ArticleController::class, 'show']);
