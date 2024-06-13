8513867c71578ebd03447ef573400526db32ba2c
Anton Korolev <antonkorolev.dev@gmail.com>
2024-06-14 00:02:59 +0300

    Add a route to test article imports

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,14 @@
 <?php
 
 use App\Http\Controllers\ArticleController;
+use Illuminate\Foundation\Http\Middleware\ValidateCsrfToken;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/article', [ArticleController::class, 'index']);
-Route::get('/article/{id}', [ArticleController::class, 'show']);
+Route::get('/articles', [ArticleController::class, 'index']);
+Route::get('/articles/{id}', [ArticleController::class, 'show']);
+
+Route::get('/article/import/', [ArticleController::class, 'import']);//->withoutMiddleware(ValidateCsrfToken::class);
